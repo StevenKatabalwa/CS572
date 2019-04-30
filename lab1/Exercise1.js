2bf8b22de7f02a5204f4fbbdd9f2495c65ac4e7d
StevenKatabalwa <StevenKatabalwa2019@gmail.com>
2019-04-29 21:09:52 -0500

    submit Ex: 1,2, and 3

--- lab1/Exercise1.js
+++ lab1/Exercise1.js
@@ -1,12 +1,16 @@
+const rxjs = require('rxjs')
+const operators = require('rxjs/operators')
 
 //using ES6
 const sentence = "This house is nice !"
 const bannedWords = ["house", "nice"]
 
-const result = sentence.split(" ").map(st => !bannedWords.includes(st)?st:'***').reduce((first, last) => `${first} ${last}`)
+const result = sentence.split(" ").map(st => !bannedWords.includes(st) ? st : '***').reduce((first, last) => `${first} ${last}`)
 
 console.log(result)
 
+
+/*----------------------------------------------------------------*/
 
 //using Promises
 String.prototype.filterWords = function (more) {
@@ -14,28 +18,61 @@ String.prototype.filterWords = function (more) {
     return new Promise((resolve, reject) => {
 
         const words = this.toString().split(" ")
-        const result=[words.length];
-        var count=0;
+        const result = [words.length];
+        let count = 0;
 
-        words.forEach(w=>{
+        words.forEach(w => {
 
-            if(more.includes(w)){
+            if (more.includes(w)) {
 
-            result[count]="***"
+                result[count] = "***"
 
             }
 
-            else{
-                result[count]=w
+            else {
+                result[count] = w
             }
-            
+
             count++;
         })
 
-        resolve(result.reduce((a,b)=>`${a} ${b}`))
-    }).then((v)=>console.log(v))
+        resolve(result)
+    }).then((v) => console.log(v.reduce((a, b) => `${a} ${b}`)))
 }
 
 sentence.filterWords(bannedWords)
 
+/*----------------------------------------------------------------*/
+
+//using Async/Await
+String.prototype.filterWords = async function (banned) {
+
+    const result = await this.toString().split(" ").map(w => !banned.includes(w) ? w : '***').reduce((first, last) => `${first} ${last}`)
+
+    return result
+}
+
+sentence.filterWords(bannedWords).then(console.log)
+
+
+/*----------------------------------------------------------------*/
+
 //Using Observables
+const { Observable } = rxjs
+const { map, reduce, flatMap } = operators
+
+String.prototype.filterWords = function (banned) {
+
+    const words$ = Observable.create((observer) => {
+        observer.next(this.toString())
+        observer.complete()
+    })
+
+    words$.pipe(
+        flatMap(x => x.split(" ")),
+        map(w => !bannedWords.includes(w) ? w : '***'),
+        reduce((first, last) => `${first} ${last}`)
+    ).subscribe(console.log)
+}
+
+sentence.filterWords(bannedWords)
--- lab1/Exercise2.js
+++ lab1/Exercise2.js
@@ -1,34 +1,52 @@
-const rxjs=require('rxjs')
-const operators=require('rxjs/operators')
+const rxjs = require('rxjs')
+const operators = require('rxjs/operators')
 
-const {of,from,Observable}=rxjs
-const {filter, map}=operators
+const { of, from, Observable } = rxjs
+const { filter, map } = operators
 
-const date=new Date()
-const day=date.getDay()
+const date = new Date()
+const day = date.getDay()
 
 
-const findWeekDay=(day)=>{
+//Approach using Data Structure (Arrays with finite size and random accessibility)
+const findOutPeriod = (day) => {
 
-    return new Promise((resolve,reject)=>{
+    const [we, wd] = ["Weekend", "Weekday"]
 
-        const data=Observable.create((x)=>{
+    const periods = [we, wd, wd, wd, wd, we, we]
+
+    return new Promise((resolve, reject) => {
+
+        resolve(periods[day])
+
+        reject("Day does not exist")
+
+    }).then(console.log)
+}
+
+findOutPeriod(day)
+
+
+/*Alternative approach using a promise and an observable:
+ Comparator involved likely to use if-else implementations.*/
+ const findWeekDay = ((day) => {
+
+    return new Promise((resolve, reject) => {
+
+        const data = Observable.create((x) => {
             x.next(day)
             x.complete()
         })
 
         data.pipe(
-            filter(d=>d==0||d==6)
-            ,map(d=>'Weekend')
-        ).subscribe(x=>resolve(x))
+            filter(d => d == 0 || d == 6)
+            , map(d => 'Weekend')
+        ).subscribe(x => resolve(x))
 
         data.pipe(
-            filter(d=>d>0&&d<=6)
-            ,map(d=>'Weekday')
-        ).subscribe(x=>reject(x))
+            filter(d => d > 0 && d <= 6)
+            , map(d => 'Weekday')
+        ).subscribe(x => reject(x))
 
     })
-}
-
-findWeekDay(day).then((d)=>console.log(d),(d)=>console.log(d))
-
+})(day).then((d) => console.log(d), (d) => console.log(d))
