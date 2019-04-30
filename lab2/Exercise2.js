10ad6c8fad9ba6d41c937c412f1b8962e1520b36
StevenKatabalwa <StevenKatabalwa2019@gmail.com>
2019-04-30 16:54:20 -0500

    added lab 2

---
+++ lab2/Exercise2.js
@@ -0,0 +1,23 @@
+
+Array.prototype.even = function () {
+
+    const func = (async () => {
+        return this.filter(x => (x % 2) == 0)
+    })().then(console.log)
+}
+
+Array.prototype.odd = function () {
+    const func = (async () => {
+        return this.filter(x => (x % 2) == 1)
+    })().then(console.log)
+}
+
+console.log('start')
+
+const a = [1, 2, 3, 4, 5, 6, 7, 8]
+const b = [1, 2, 3, 4, 5, 6, 7, 8]
+
+a.even()
+b.odd()
+
+console.log('end')
