505ed1922a00fffed009ca0a364cd46f77d6ade1
StevenKatabalwa <StevenKatabalwa2019@gmail.com>
2019-05-03 00:03:04 -0500

    changes

--- lab4/child.js
+++ lab4/child.js
@@ -1,26 +1,53 @@
 const fs = require('fs')
+const http = require('http')
+const url = require('url')
+const env = require('dotenv')
 
-process.on('message', (path) => {
+env.config({ path: '../lab4/.env' })
 
-    let file=undefined
+const portNumber = process.env.PORT
+const hostName = process.env.HOSTNAME
 
-    fileExists(path, (result) => {
 
-        if (result) {
-            console.log('File exists')          
-        }
-        else {
-            console.log('File Not Exist')         
-            fs.writeFileSync(path,"This is the new file i have created",()=>{
-                console.log(`File ${path} has been created`)
+process.on('message', () => {
+
+    let file = undefined
+
+    http.createServer((req, res) => {
+
+        const query = url.parse(req.url).path
+
+        var [tag, file] = query.split("=")
+
+        file = file || 'text.txt'
+
+        fileExists(file, (result) => {
+
+            if (result) {
+                console.log('File exists')
+            }
+            else {
+                console.log('File Not Exist')
+                fs.writeFileSync(file, "This is the new file i have created", () => {
+                    console.log(`File ${file} has been created`)
+                })
+            }
+
+            file = fs.readFile(file, (err, data) => {
+
+                res.write(data.toString())
+                res.end()
+
+                process.send(`Job has been completed by Processer: ${process.pid}`)
+
             })
-        }
 
-        file=fs.readFile(path,(err,data)=>{
-                process.send(data.toString())
-                process.exit()
+            res.on('close', () => {
+            
+            })
         })
-    })
+
+    }).listen(portNumber, hostName).on('listening', () => console.log('Server is listening'))
 
 })
 
