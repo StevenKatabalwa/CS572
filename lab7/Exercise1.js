3f5ea7c6a284e5b7c5b3d35f63a6373902da6837
StevenKatabalwa <StevenKatabalwa2019@gmail.com>
2019-05-07 23:49:11 -0500

    mem

---
+++ lab7/Exercise1.js
@@ -0,0 +1,26 @@
+const client = require('./mongoClient')
+const env = require('dotenv')
+const express = require('express')
+
+env.config()
+
+const hostName = process.env.HOSTNAME
+const port = process.env.PORT
+
+console.log(hostName+port)
+
+const app = express()
+const router = express.Router()
+
+app.use('/', client)
+
+router.get('/', (req, res, next) => {
+
+      
+
+})
+
+app.listen(port, hostName)
+    .on('listening', () => {
+        console.log('Server is listening')
+    })
